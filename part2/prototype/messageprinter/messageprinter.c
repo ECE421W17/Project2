ae95830bdb6725573e2aea1c310ce4722b05486b
Derek Shultz <shultz.derek@gmail.com>
2017-02-25 22:58:52 -0700

    Changed to use non-obsolete nanoseconds function for waiting.

--- part2/prototype/messageprinter/messageprinter.c
+++ part2/prototype/messageprinter/messageprinter.c
@@ -1,7 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 
 void wait_and_print(unsigned time, char * msg) {
-    sleep(time);
-    printf("%s\n", msg);
+    //pid_t f;
+    //f = fork();
+    //if (f==0) {
+        struct timespec time_amount;
+        time_amount.tv_sec = time/1000;
+        time_amount.tv_nsec = (long) (time % 1000) * 1000000;
+        nanosleep(&time_amount, NULL);
+        //sleep(time);
+        printf("\n%s\n", msg);
+    //}
 }
