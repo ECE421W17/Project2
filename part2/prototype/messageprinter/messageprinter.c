4acf6a255a150a49da9482f1813a714b185664ca
Derek Shultz <shultz.derek@gmail.com>
2017-02-28 14:16:00 -0700

    Added better error checking. Changed a few interface things around.

--- part2/prototype/messageprinter/messageprinter.c
+++ part2/prototype/messageprinter/messageprinter.c
@@ -1,11 +1,20 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <errno.h>
 
-void wait_and_print(unsigned time, char * msg) {
+int wait_and_print(long sec, long nsec, char * msg) {
     struct timespec time_amount;
-    time_amount.tv_sec = time/1000; // Get seconds
-    time_amount.tv_nsec = (long) (time % 1000) * 1000000; // Get milliseconds
+    time_amount.tv_sec = sec; // Get seconds
+    time_amount.tv_nsec = nsec; // Get milliseconds
     nanosleep(&time_amount, NULL);
+    if (errno == 4 || errno == 14 || errno == 22) {
+      return -1;
+    }
     printf("\n%s\n", msg);
+    return 0;
 }
+
+// int main() {
+//   wait_and_print(1, 1, "yo");
+// }
